6ae6435ec99c812b72de59f24072cc63b2893872
drishtish-13 <125680145+drishtish-13@users.noreply.github.com>
2024-06-14 19:06:29 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,6 @@
+const accordion=document.getElementsByClassName('content-container');
+for(i=0;i<accordion.length;i++){
+    accordion[i].addEventListener('click',function(){
+        this.classList.toggle('active');
+    })
+};
